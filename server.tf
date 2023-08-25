be0c698a0e711fbe10fbad3a9518b27414ec9e2a
Mahib21 <143081715+Mahib21@users.noreply.github.com>
2023-08-25 12:38:46 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -11,7 +11,7 @@ provider "aws" {
 region = "us-west-1"
 }
 resource "aws_instance" "myawsserver" {
-  ami = "ami-09c72d5a7ce529ad5"
+  ami = "ami-026257f4f39c28af8"
   instance_type = "t2.micro"
   key_name = "mahesh-import"
 
